1a9dba8d6167b48943bbcc18ca1878b3d84add5b
vvh0ame <77536370+vvh0ame@users.noreply.github.com>
2024-10-11 21:53:51 +0500

    Update and rename riddles_api.py to riddles_api.js

---
+++ src/riddles_api.js
@@ -0,0 +1,19 @@
+class RiddlesApi {
+	constructor() {
+		this.api = "https://riddles-api.vercel.app"
+		this.headers = {
+			"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.5060.114 Safari/537.36"
+		}
+	}
+
+	async getRandoRiddle() {
+		const response = await fetch(
+			`${this.api}/random`, {
+				method: "GET",
+				headers: this.headers
+			})
+		return response.json()
+	}
+ }
+
+module.exports = {RiddlesApi}
